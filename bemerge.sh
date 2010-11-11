769cedb446cd53f285ce5dc59ec209fd3a2ad76c
write2david <write2david@gmail.com>
2010-11-10 23:21:34 -0600

    Variety of updates

--- bemerge.sh
+++ bemerge.sh
@@ -18,33 +18,45 @@ compile() {
 
 notice() {
                 # String all these lines/commands together with "&&" so that if the user hits Ctrl-C anytime during the post-emerge process, then the whole post-emerge process dies.  But this method doesn't work on lines that have pipes  ("|") and so don't use it on those lines.
-		einfo "Clean up: remove non-English/French locale files & remove unneeded Portage install files..."
+		einfo "" && \
+		einfo "Clean up: non-local locale files..." && \
                 localepurge | grep freed
 		#
+		einfo "Remove unneeded Portage install files..." && \
 		# -d is not too destructive and without it you can have lots of unneccesary source code install files building up
 		eclean-dist -d && \
 		#
-		einfo "Updating list of modules provided by ebuilds..." && \
+		einfo "Updating list of kernel modules provided by installed packages..." && \
 		# From: http://www.gentoo.org/doc/en/kernel-upgrade.xml#doc_chap6
 		# must emerge module-rebuild for this to work
 		[ -f /usr/sbin/module-rebuild ] && module-rebuild populate > /dev/null && \
 		einfo "" && \
 		einfo "" &&\
 		einfo "You may also want to run:" && \
-		einfo "         revdep-rebuild -- -av  (reverse dependency check)" && \
-		einfo "         dispatch-conf  (update configuration files)" && \
-		einfo "         module-rebuild rebuild  (rebuild kernel modules provided by ebuilds)" && \
-		einfo "         bemerge --prune -av  (remove old versions from multiple-slots" && \
-		einfo "         eclean-dist -d (to only keep minimum for reinstallation)." && \
-		einfo "         bemerge --depclean -av  (remove packages not needed anymore)" && \
-		einfo "         python-updater  (for any updates of python)" && \
-		einfo "         eselect  (for any updates of kernel, python, java)" && \
-		einfo "         elogv or  eread  (review elog messages for newly installed packages)" && \
+		einfo "" && \
+		einfo "revdep-rebuild -- -av      (reverse dependency check)" && \
+		einfo "dispatch-conf              (update configuration files)" && \
+		einfo "module-rebuild rebuild     (rebuild kernel modules provided by ebuilds)" && \
+		einfo "emerge --prune -av         (remove old versions from multiple-slots" && \
+		einfo "emerge --depclean -av      (remove packages not needed anymore)" && \
+		einfo "python-updater             (for any updates of python)" && \
+		einfo "perl-cleaner --all         (for any updates of perl)" && \
+		einfo "java-check-environment     (for any java installs)" && \
+		einfo "eselect                    (for any updates involving choices)" && \
+		einfo "eix-test-obsolete detailed (for checking your /etc/portage files for unused entries)" && \
+		einfo "elogv [or] eread           (review elog messages for newly installed packages)" && \
+		einfo "mysql_upgrade -p           (for any mySQL program upgrades -- then restart mysqld)" && \
+		#
+		# next line requires package "portage-utils"
+		einfo "emerge -1av \$(qlist -IC x11-drivers/)   (to rebuild drivers, after upgrading xorg-sever)" && \
+		#
 		einfo "" && \
 		#	The next line came from content on:  http://www.gentoo.org/doc/en/gcc-upgrading.xml
-		einfo "         If you've upgraded GCC, then do:  gcc-config i686-pc-linux-gnu-X.X.X && env-update && source /etc/profile     (use your new version for X.X.X)" && \
-		einfo "            ... and then:  emerge --oneshot -av libtool && emerge -eav system && emerge -eav world" && \
-		einfo "            ... and then remove the old version of GCC." && \
+		einfo "If you've upgraded GCC, then do:  gcc-config i686-pc-linux-gnu-X.X.X && env-update && source /etc/profile       (use your new version for X.X.X)" && \
+		einfo "   ... and then:  emerge -1av libtool && emerge -eav system && emerge -eav world" && \
+		einfo "   ... and then remove the old version of GCC." && \
+		einfo "" && \
+		einfo "Occasionally, do this to see what upgrades have been missed when doing a world upgrade, which may also prohibit emerge --prune/--depclean from working:  'emerge -DavuNt1e world'." && \
 		einfo "" && \
 		einfo "" && \
 		#
